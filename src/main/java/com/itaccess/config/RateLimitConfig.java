b6027feebc69c9543189fe67fc3412cf1a3b519a
nkanusephora00-droid <nkanusephora00@gmail.com>
2026-05-05 09:07:54 +0100

    Add files via upload

--- src/main/java/com/itaccess/config/RateLimitConfig.java
+++ src/main/java/com/itaccess/config/RateLimitConfig.java
@@ -16,8 +16,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class RateLimitConfig implements Filter {
     
-    private static final int MAX_REQUESTS = 50;
-    private static final long TIME_WINDOW_MS = 60000; // 1 minute
+    private static final int MAX_REQUESTS = 70;
+    private static final long TIME_WINDOW_MS = 160000; // 1 minute
     
     private final ConcurrentHashMap<String, RateLimitInfo> rateLimitMap = new ConcurrentHashMap<>();
     
